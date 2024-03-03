8d754979bb157f935aca4a4123b2f7e9bdad4ef0
Paolo Bartolucci <paolobarto210@gmail.com>
2024-03-02 20:47:20 -0500

    14

--- RxPicker/src/app/app-routing.module.ts
+++ RxPicker/src/app/app-routing.module.ts
@@ -8,3 +8,25 @@ const routes: Routes = [];
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
+
+import { HomePageComponent } from './components/home-page/home-page.component'
+import { TracerPageComponent } from './components/tracer-page/tracer-page.component'
+import { SensitivityPageComponent } from './components/sensitivity-page/sensitivity-page.component'
+import { AboutPageComponent } from './components/about-page/about-page.component'
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: HomePageComponent},
+  { path: 'tracer', component: TracerPageComponent},
+  { path: 'sensitivity', component: SensitivityPageComponent},
+  { path: 'about', component: AboutPageComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
